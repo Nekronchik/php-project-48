23d9b4cbea7e73b11f4ef16f8e4fd76052b5dc16
Andrey Gaponov <mr.nekron@gmail.com>
2024-09-26 21:54:40 +0300

    fix lint in tests

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 use function Differ\Differ\genDiff;
 
-    class DifferTest extends TestCase
+class DifferTest extends TestCase
 {
     public function testGendiff(): void
     {
